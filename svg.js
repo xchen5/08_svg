698f02e6094a4cb9ac5bcd5340ade55097ccad35
Xavier Chen <xchen5@stuy.edu>
2018-03-03 14:35:33 -0500

    finished

--- svg.js
+++ svg.js
@@ -1,10 +1,15 @@
 var pic = document.getElementById("a");
-var clear = document.getElementById("clear");
+var stop = document.getElementById("stop");
 var circleButton = document.getElementById("circle");
+var dvdButton = document.getElementById("dvdButton");
 
 var requestID;
 var toEnlarge = true;
+var toXIncrease = true;
+var toYIncrease = true;
 var x = 0;
+var DVDx = 250;
+var DVDy = 250;
 
 var circle = function(){
     pic.innerHTML = ' ';
@@ -24,13 +29,71 @@ var circle = function(){
     };
   };
   c1.setAttribute("r", x);
-  pic.appendChild(c1)
-  console.log(requestID)
+  pic.appendChild(c1);
+  console.log(requestID);
 };
 
+var toStop = function(){
+  clearInterval(requestID);
+}
+
 var toCircle = function(){
-    clearInterval(circle);
+    toStop();
+    pic.innerHTML = ' ';
+    x = 0;
     requestID = setInterval(circle, 10);
 };
 
+var dvd = function(){
+  pic.innerHTML= ' ';
+  var c1 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  c1.setAttribute("x", DVDx);
+  c1.setAttribute("y", DVDy);
+  c1.setAttribute("width", 50);
+  c1.setAttribute("height", 30);
+  if (toXIncrease){
+    DVDx += 1;
+    if(toYIncrease){
+      DVDy += 2;
+    }
+    else{
+      DVDy-= 2;
+    }
+  }
+  else{
+    DVDx -= 1;
+    if(toYIncrease){
+      DVDy += 2;
+    }
+    else{
+      DVDy-= 2;
+    }
+  }
+  if(DVDx >= 450) {
+    toXIncrease = false;
+  }
+  if(DVDx <= 0){
+    toXIncrease = true;
+  }
+  if(DVDy >= 470){
+    toYIncrease = false;
+  }
+  if(DVDy <= 0){
+    toYIncrease = true;
+  }
+  pic.appendChild(c1);
+};
+
+var toDvd = function(){
+  toStop();
+  pic.innerHTML = ' ';
+  toXIncrease = true;
+  toYIncrease = true;
+  DVDx = 250;
+  DVDy = 250;
+  requestID = setInterval(dvd, 10);
+}
+
+stop.addEventListener("click", toStop);
 circleButton.addEventListener("click", toCircle);
+dvdButton.addEventListener("click", toDvd);
